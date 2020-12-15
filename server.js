2eefa5d6132170435d711f996e689988444fd357
Lucas Kempe <lucas.kempe@icloud.com>
2020-12-15 19:12:43 -0300

    New mongoose database...

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const dotenv = require('dotenv')
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config()
+}
+
 const express = require ('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
@@ -10,6 +15,13 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }, { useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 
