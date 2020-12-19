09795f38c0a3493b0399856d0bbad2f1f4f4fc93
Lucas Kempe <lucas.kempe@icloud.com>
2020-12-19 10:38:46 -0300

    Adding navbar to the project without style xd...

---
+++ models/signup.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const userName = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Name', userName)
